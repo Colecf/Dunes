22c8d6bebca099e300479a6d9e372e08e5aaad0c
Francis Cruz <frcruz@ucsc.edu>
2017-10-20 12:30:30 -0700

    Added 3 panels with some clicking functionality

--- Dunes/mainwindow.cpp
+++ Dunes/mainwindow.cpp
@@ -5,9 +5,85 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    //Initilize UI
+    //Module list
+    mod_list = new QListWidget(this);
+    mod_list->resize(320, 360);
+    //Add item to list
+    mod_list->addItem("Select something");
+    mod_list->item(mod_list->count() - 1)->setToolTip("This is my description!");
+    mod_list->addItem("LOOP");
+    mod_list->item(mod_list->count() - 1)->setToolTip("This is a loop description");
+
+    //Description box
+    desc = new QTextEdit("Click on a module to see its description!", this);
+    desc->move(0, 360);
+    desc->resize(320, 360);
+    desc->setReadOnly(true);
+
+    tree = new QTreeWidget(this);
+    tree->move(320, 0);
+    tree->resize(640, 540);
+    //columns index start at 0 like arrays.
+    tree->setColumnCount(1);
+    //Connects the module list with the description
+    connect(mod_list, SIGNAL(itemSelectionChanged()), this, SLOT( update_desc()));
+
+    //Connects module list with tree only on double click
+    connect(mod_list, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(update_tree()));
+
+    //Remove item from tree by double clicking
+    connect(tree, SIGNAL (itemDoubleClicked(QTreeWidgetItem*,int)), this, SLOT(remove_entry_from_tree()));
+
+    //Setting up description table on click change the description the new module description
+
+
+    //ui->setupUi(this);
 }
 
+void MainWindow::update_desc()
+{
+   desc->setText(mod_list->currentItem()->toolTip());
+}
+
+void MainWindow::update_tree()
+{
+    QStringList *cur_item = new QStringList(mod_list->currentItem()->text());
+
+    QTreeWidgetItem *new_tree_item = new QTreeWidgetItem();
+    new_tree_item->setText(0, mod_list->currentItem()->text());
+    tree->addTopLevelItem(new_tree_item);
+    //stuff for proof of concept
+    QTreeWidgetItem *child_tree_item = new QTreeWidgetItem();
+    child_tree_item->setText(0, "child testing!!!");
+    new_tree_item->addChild(child_tree_item);
+
+    QTreeWidgetItem *child_tree_item1 = new QTreeWidgetItem();
+    child_tree_item1->setText(0, "its me ur brother");
+    new_tree_item->addChild(child_tree_item1);
+    QTreeWidgetItem *great_child_tree_item = new QTreeWidgetItem();
+    great_child_tree_item->setText(0, "young dude here");
+    child_tree_item1->addChild(great_child_tree_item);
+
+
+
+
+
+}
+//delete entrys from tree
+void MainWindow::remove_entry_from_tree()
+{
+    QTreeWidgetItem *cur = tree->currentItem();
+    if(tree->currentItem()->parent() != NULL)
+    {
+        tree->currentItem()->parent()->removeChild(cur);
+        free(cur);
+    }
+    else
+    {
+        delete(cur);
+    }
+}
 MainWindow::~MainWindow()
 {
     delete ui;
--- Dunes/mainwindow.h
+++ Dunes/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QListWidget>
+#include <QTableWidget>
+#include <QTextEdit>
+#include <QTreeWidget>
 
 namespace Ui {
 class MainWindow;
@@ -10,13 +14,19 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+private slots:
+    void update_desc();
+    void update_tree();
+    void remove_entry_from_tree();
 private:
     Ui::MainWindow *ui;
+    QListWidget *mod_list;
+    QTableWidget *desc_table;
+    QTextEdit *desc;
+    QTreeWidget *tree;
 };
 
 #endif // MAINWINDOW_H
