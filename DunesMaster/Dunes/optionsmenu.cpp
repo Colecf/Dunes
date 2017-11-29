ef0be3f015227e93600fa05aeea98db0db44d4d1
Derek <derek_kwong8@yahoo.com>
2017-11-29 12:43:59 -0800

    finished options menu

--- DunesMaster/Dunes/optionsmenu.cpp
+++ DunesMaster/Dunes/optionsmenu.cpp
@@ -1,4 +1,5 @@
 #include <QDialogButtonBox>
+#include <QDebug>
 #include "optionsmenu.h"
 
 OptionsMenu::OptionsMenu()
@@ -6,16 +7,46 @@ OptionsMenu::OptionsMenu()
     nodePath = "";
     npmPath = "";
     layout = new QVBoxLayout();
+    nodeLayout = new QHBoxLayout();
+    npmLayout = new QHBoxLayout();
     this->setLayout(layout);
     this->setModal(true);
+    nodeLabel = new QLabel("Node Path:");
+    npmLabel = new QLabel("NPM Path: ");
+    nodeLineEdit = new QLineEdit();
+    npmLineEdit = new QLineEdit();
+    nodeLayout->addWidget(nodeLabel);
+    nodeLayout->addWidget(nodeLineEdit);
+    npmLayout->addWidget(npmLabel);
+    npmLayout->addWidget(npmLineEdit);
     QDialogButtonBox *buttons = new QDialogButtonBox(QDialogButtonBox::Save | QDialogButtonBox::Cancel);
+    layout->addLayout(nodeLayout);
+    layout->addLayout(npmLayout);
     layout->addWidget(buttons);
-    connect(buttons, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(buttons, SIGNAL(accepted()), this, SLOT(saveChanges()));
     connect(buttons, &QDialogButtonBox::rejected, this, &QDialog::reject);
 }
 
 void OptionsMenu::showOptions(){
     this->show();
+}
+
+void OptionsMenu::saveChanges(){
+    setNodePath(nodeLineEdit->text());
+    setNpmPath(npmLineEdit->text());
+    qInfo() << "node path is: " << nodePath;
+    qInfo() << "npm path is: " << npmPath;
+    this->done(0);
+}
+
+void OptionsMenu::setNodePath(QString path){
+    nodeLineEdit->setText(path);
+    nodePath = path;
+}
+
+void OptionsMenu::setNpmPath(QString path){
+    npmLineEdit->setText(path);
+    npmPath = path;
 }
 
 QString OptionsMenu::getNodePath(){
--- DunesMaster/Dunes/optionsmenu.h
+++ DunesMaster/Dunes/optionsmenu.h
@@ -1,5 +1,7 @@
 #include <QDialog>
 #include <QString>
+#include <QLabel>
+#include <QLineEdit>
 #include <QVBoxLayout>
 
 #ifndef OPTIONSMENU_H
@@ -11,12 +13,21 @@ class OptionsMenu : public QDialog
     Q_OBJECT
 public:
     OptionsMenu();
+    void setNpmPath(QString path);
+    void setNodePath(QString path);
     QString getNodePath();
     QString getNpmPath();
 private slots:
     void showOptions();
+    void saveChanges();
 private:
+    QLabel *nodeLabel;
+    QLineEdit *nodeLineEdit;
+    QLabel *npmLabel;
+    QLineEdit *npmLineEdit;
     QVBoxLayout *layout;
+    QHBoxLayout *nodeLayout;
+    QHBoxLayout *npmLayout;
     QString nodePath;
     QString npmPath;
 };
