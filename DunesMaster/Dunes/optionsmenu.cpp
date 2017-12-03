08246714aa7fb57d85e4a84c9733ba28cfa1582c
Derek <derek_kwong8@yahoo.com>
2017-12-02 16:27:25 -0800

    Added documentation for optionsmenu

--- DunesMaster/Dunes/optionsmenu.cpp
+++ DunesMaster/Dunes/optionsmenu.cpp
@@ -5,6 +5,12 @@
 #include <QStandardPaths>
 #include "optionsmenu.h"
 
+/*
+    Sets up the layout for the OptionsMenu. Inherits from QDialog.
+    Sets a vertical layout, then for the node and npm label and line edits, a horizontal layout.
+    Adds the label, line edit to respective layouts. Adds save and cancel buttons, and sets the connection
+    for both buttons.
+*/
 OptionsMenu::OptionsMenu()
 {
     nodePath = "";
@@ -30,10 +36,16 @@ OptionsMenu::OptionsMenu()
     connect(buttons, &QDialogButtonBox::rejected, this, &QDialog::reject);
 }
 
+/*
+    Called on clicking the Options button. Shows this dialog.
+*/
 void OptionsMenu::showOptions(){
     this->show();
 }
 
+/*
+    Called on clicking the Ok button. Saves the paths set in the line edits, and closes the dialog.
+*/
 void OptionsMenu::saveChanges(){
     setNodePath(nodeLineEdit->text());
     setNpmPath(npmLineEdit->text());
@@ -42,11 +54,17 @@ void OptionsMenu::saveChanges(){
     this->done(0);
 }
 
+/*
+    Sets the line edit's text to path, and sets nodePath to path.
+*/
 void OptionsMenu::setNodePath(QString path){
     nodeLineEdit->setText(path);
     nodePath = path;
 }
 
+/*
+    Sets the line edit's text to path, and sets nodePath to path.
+*/
 void OptionsMenu::setNpmPath(QString path){
     npmLineEdit->setText(path);
     npmPath = path;
